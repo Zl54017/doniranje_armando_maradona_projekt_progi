68418afb861ae3d881062329247c541c715fe82e
NikolaBorzic <148991849+NikolaBorzic@users.noreply.github.com>
2023-12-25 11:07:58 +0100

    atribut city
    dodan atribut grad za bloodbank

--- server/models/bloodbank.js
+++ server/models/bloodbank.js
@@ -19,6 +19,7 @@ module.exports = (sequelize, DataTypes) => {
       email: DataTypes.STRING,
       password: DataTypes.STRING,
       address: DataTypes.STRING,
+      city: DataTypes.STRING,
       numberOfDonors: DataTypes.INTEGER,
     },
     {
